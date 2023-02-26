2d2e8e04d54134d73002d44794393bb63d10237b
Secret1337 <97087168+secret-pommes@users.noreply.github.com>
2023-02-26 20:44:09 +0100

    notice in news and lightswitch, keychains, cosmetics

--- struct/endpoints.js
+++ struct/endpoints.js
@@ -2,21 +2,42 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-// News in launcher
+// News and text under it (page 2) in launcher
 app.get("/public/Launcher/launcher-defaultstrings.json", (req, res) => {
   var json = path.join(__dirname, "/../json/launcher-defaultstrings.json");
   res.sendFile(json);
 });
 
-// Home / pictures in launcher background
+// Home | pictures in launcher background
 app.get("/public/Launcher/launcher-userinterfacesettings.json", (req, res) => {
-  var json = path.join(__dirname, "/../json/launcher-userinterfacesettings.json");
+  var json = path.join(
+    __dirname,
+    "/../json/launcher-userinterfacesettings.json"
+  );
   res.sendFile(json);
 });
 
-// Content pages in fortnite (eracen.danihhhhh.repl.co)
+// Content pages in fortnite
 app.get("/public/fortnite-game.json", (req, res) => {
   var json = path.join(__dirname, "/../json/fortnite-game.json");
+  res.sendFile(json);
+});
+
+// List with active skins (Locker)
+app.get("/public/cosmetics.json", (req, res) => {
+  var json = path.join(__dirname, "/../json/cosmetics.json");
+  res.sendFile(json);
+});
+
+// Keychain for project era (1.11 up to 12.41?)
+app.get("/publickeychain.json", (req, res) => {
+  var json = path.join(__dirname, "/../json/public/keychain.json");
+  res.sendFile(json);
+});
+
+// Lightswitch (server-status, bans)
+app.get("/public/status.json", (req, res) => {
+  var json = path.join(__dirname, "/../json/lightswitch.json");
   res.sendFile(json);
 });
 
