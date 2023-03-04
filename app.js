a74200b9f34369d7d36267a0f3e037fbecab7c08
Secret1337 <97087168+secret-pommes@users.noreply.github.com>
2023-03-04 21:19:35 +0100

    added EraV1 Support!
    EraV1:
    Backend now supports:
    Era Alpha (pre-beta)
    Era Beta
    Era Release 1.0 up to 1.5
    
    Adding a backend for 1.0 up to 1.5 soon

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const path = require("path");
 const config = require("./config/serverconfig.json");
 
 var serverport = config.Server.ServerPort;
@@ -19,6 +18,7 @@ app.listen(serverport, () => {
     }
 });
 
-app.use(require("./struct/endpoints.js"))
+app.use(require("./struct/endpoints.js"));
+app.use(require("./struct/EraV1.js"));
 // KEEP THIS AT THE END OF THE CODE (if you ignore it the backend will not work)
 app.use(require("./struct/errors.js"));
--- struct/errors.js
+++ struct/errors.js
@@ -1,23 +1,31 @@
 const express = require("express");
 const app = express();
+const config = require("../config/serverconfig.json");
 
-app.get("*", (req, res) => {
-  var XEpicErrorName = "errors.com.epicgames.common.not_found";
-  var XEpicErrorCode = 1004;
 
-  res.set({
-    "X-Epic-Error-Name": XEpicErrorName,
-    "X-Epic-Error-Code": XEpicErrorCode,
+if(config["ERA-V1"]["Support Old Era"] == true){
+  app.get("*", (req, res)=>{
+    res.redirect(config["ERA-V1"]["ERA-V1 Backend"]+"/*")
+  })
+} else{
+  app.get("*", (req, res) => {
+    var XEpicErrorName = "errors.com.epicgames.common.not_found";
+    var XEpicErrorCode = 1004;
+  
+    res.set({
+      "X-Epic-Error-Name": XEpicErrorName,
+      "X-Epic-Error-Code": XEpicErrorCode,
+    });
+    res.status(404); // sends 404
+    res.json({
+      errorcode: XEpicErrorName,
+      errormessage:
+        "Sorry the resource you were trying to find could not be found",
+      numericErrorCode: XEpicErrorCode,
+      originatingService: "any",
+      intent: "prod",
+    });
   });
-  res.status(404); // sends 404
-  res.json({
-    errorcode: XEpicErrorName,
-    errormessage:
-      "Sorry the resource you were trying to find could not be found",
-    numericErrorCode: XEpicErrorCode,
-    originatingService: "any",
-    intent: "prod",
-  });
-});
+}
 
 module.exports = app;
